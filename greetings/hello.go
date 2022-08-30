ea2cec8572559a51cbca693fa22769fa85750487
Rubén Torrero Marijnissen <rtorreromarijnissen@suse.com>
2022-08-30 15:46:33 +0100

    New version ready

--- greetings/hello.go
+++ greetings/hello.go
@@ -5,6 +5,6 @@ import "fmt"
 // Hello returns a greeting for the named person.
 func Hello(name string) string {
     // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome to the NEW version!", name)
+    message := fmt.Sprintf("Hi, %v. Welcome to the NEW NEW version!", name)
     return message
 }
